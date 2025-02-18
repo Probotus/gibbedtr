1ee129b814c933ccd7fd53eecfabec893e885e19
Probotus <BI5iqkda@t-online.de>
2025-02-18 21:45:48 +0100

    feat: platfrom switch to x64

--- Gibbed.TombRaider.DRMEdit/Properties/AssemblyInfo.cs
+++ Gibbed.TombRaider.DRMEdit/Properties/AssemblyInfo.cs
@@ -27,18 +27,6 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Gibbed.TombRaider.DRMEdit")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Gibbed.TombRaider.DRMEdit")]
-[assembly: AssemblyCopyright("Copyright © Gibbed 2011")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -46,16 +34,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("011ac9cc-86da-48a3-9d63-2da472ebf893")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
